4731954134067b5dda4a79caf353ed36583fb9c0
kim <wjzheng@users.noreply.github.com>
2017-06-07 16:45:03 +0800

    init

--- app.js
+++ app.js
@@ -4,11 +4,6 @@
 
 var app = require('koa')()
   , path = require('path')
-  , cors = require('koa-cors')
-  , json = require('koa-json')
-  , session = require('koa-session')
-  , bodyParser = require('koa-bodyparser')
-  , send = require('koa-send')
   , appRouter = require('./router')
   , EJSRender = require('koa-ejs')
   , debug = require('debug')('react:app')
@@ -28,26 +23,8 @@ EJSRender(app, {
   cache: false,
   debug: true
 });
-//cookie的加密串
-app.keys = ['HBEC'];
-app.use(session({
-  //session 名称
-  key: 'HBEC_SESSIONID',
-  //过期时间:半个小时
-  maxAge: 30 * 60 * 1000
-}, app));
-app.use(bodyParser());
-app.use(json());
-app.use(cors());
 
 app.use(appRouter);
-
-app.use(function*(next) {
-  var pathUrl = this.path;
-  yield send(this, pathUrl, {
-    root: path.resolve(__dirname, '../')
-  })
-});
 
 //处理页面未找到异常
 app.use(function*(next) {
