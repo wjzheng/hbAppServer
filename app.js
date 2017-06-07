17bbeeca87a8cc0d788b92bb14cdbb6d35829e68
kim <wjzheng@users.noreply.github.com>
2017-06-07 16:50:30 +0800

    .

--- app.js
+++ app.js
@@ -3,10 +3,6 @@
  */
 
 var app = require('koa')()
-  , path = require('path')
-  , appRouter = require('./router')
-  , EJSRender = require('koa-ejs')
-  , debug = require('debug')('react:app')
   , AV = require('leanengine');
 AV.init({
   appId: process.env.LEANCLOUD_APP_ID,
@@ -14,30 +10,15 @@ AV.init({
   masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
 });
 app.use(AV.koa());
-var viewRoot = path.join(__dirname, 'www');
-app.viewRoot = viewRoot;
-//EJS模版引擎的配置
-EJSRender(app, {
-  root: viewRoot,
-  viewExt: 'html',
-  cache: false,
-  debug: true
-});
 
-app.use(appRouter);
-
-//处理页面未找到异常
-app.use(function*(next) {
-  if (this.status == 404) {
-    this.body = '404'
+app.use(function *(next) {
+  if (this.url === '/') {
+    // https://github.com/tj/co-views
+    yield coViews('views')('index', {title: 'Hello world'});
   } else {
     yield next;
   }
 });
 
-//监听服务端异常信息
-app.on('error', function (err) {
-  debug(err);
-});
 app.listen(process.env.LEANCLOUD_APP_PORT || '8080');
 
