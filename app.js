dd5d2f07417cf4c8edd803d43a3795a7ae96ee5f
kim <wjzheng@users.noreply.github.com>
2017-06-07 16:52:36 +0800

    .

--- app.js
+++ app.js
@@ -2,23 +2,5 @@
  * Created by kim on 2017/5/10.
  */
 
-var app = require('koa')()
-  , AV = require('leanengine');
-AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
-});
-app.use(AV.koa());
-
-app.use(function *(next) {
-  if (this.url === '/') {
-    // https://github.com/tj/co-views
-    yield coViews('views')('index', {title: 'Hello world'});
-  } else {
-    yield next;
-  }
-});
-
-app.listen(process.env.LEANCLOUD_APP_PORT || '8080');
+console.log('start leancloud app')
 
