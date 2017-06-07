7da3500de718f4b557fd62c590acf8e55e7f03e9
kim <wjzheng@users.noreply.github.com>
2017-06-07 16:17:13 +0800

    init

---
+++ app.js
@@ -0,0 +1,59 @@
+/**
+ * Created by kim on 2017/5/10.
+ */
+
+var app = require('koa')()
+  , path = require('path')
+  , cors = require('koa-cors')
+  , json = require('koa-json')
+  , session = require('koa-session')
+  , bodyParser = require('koa-bodyparser')
+  , send = require('koa-send')
+  , appRouter = require('./router')
+  , EJSRender = require('koa-ejs')
+  , debug = require('debug')('react:app');
+var viewRoot = path.join(__dirname, 'www');
+app.viewRoot = viewRoot;
+//EJS模版引擎的配置
+EJSRender(app, {
+  root: viewRoot,
+  viewExt: 'html',
+  cache: false,
+  debug: true
+});
+//cookie的加密串
+app.keys = ['HBEC'];
+app.use(session({
+  //session 名称
+  key: 'HBEC_SESSIONID',
+  //过期时间:半个小时
+  maxAge: 30 * 60 * 1000
+}, app));
+app.use(bodyParser());
+app.use(json());
+app.use(cors());
+
+app.use(appRouter);
+
+app.use(function*(next) {
+  var pathUrl = this.path;
+  yield send(this, pathUrl, {
+    root: path.resolve(__dirname, '../')
+  })
+});
+
+//处理页面未找到异常
+app.use(function*(next) {
+  if (this.status == 404) {
+    this.body = '404'
+  } else {
+    yield next;
+  }
+});
+
+//监听服务端异常信息
+app.on('error', function (err) {
+  debug(err);
+});
+module.exports = app;
+
---
+++ router.js
@@ -0,0 +1,57 @@
+/**
+ * Created by kim on 2017/5/10.
+ */
+
+var Router = require('koa-router');
+var router = new Router().prefix('/api');
+var debug = require('debug')('react:router');
+
+/**
+ * 通用接口
+ */
+router.all('/common', function*() {
+  var result = yield new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve({isSuc: true})
+    }, 500)
+  });
+  this.body = result;
+});
+
+router.get('/leancloud', function*() {
+  yield this.render('leancloud', {
+    layout: false
+  })
+});
+
+/**
+ * 用户认证
+ */
+router.post('/auth', function*() {
+  var loginId = this.request.body.loginId;
+  var password = this.request.body.password;
+  var flag = loginId === '18016052872' && password === '111111';
+  debug(`doLogin,loginId:${loginId} password:${password}`);
+  //将loginID保存到session中
+  if (flag) {
+    this.session.logonId = loginId;
+  }
+  this.body = {
+    code: 0,
+    isSuc: flag
+  }
+});
+
+/**
+ * 退出
+ */
+router.post('/logout', function*() {
+  debug(`doLogout`);
+  this.body = {
+    code: 0,
+    isSuc: true
+  }
+});
+
+
+module.exports = router.routes()
