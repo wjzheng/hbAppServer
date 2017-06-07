00f835e91086e405cba7fd2f5e68b6f0bd665607
kim <wjzheng@users.noreply.github.com>
2017-06-07 16:57:52 +0800

    .

--- app.js
+++ app.js
@@ -2,5 +2,56 @@
  * Created by kim on 2017/5/10.
  */
 
-console.log('start leancloud app')
+var app = require('koa')()
+  , path = require('path')
+  , cors = require('koa-cors')
+  , json = require('koa-json')
+  , session = require('koa-session')
+  , bodyParser = require('koa-bodyparser')
+  , send = require('koa-send')
+  , appRouter = require('./router')
+  , EJSRender = require('koa-ejs')
+  , debug = require('debug')('react:app')
+  , AV = require('leanengine');
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'G1ooxgUmbxYegIkn1oF5cN71-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'eDD28ddQiqUzF53Fl198PXQb',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'KSKQidkuq1EwSnVEUH4ipJKV'
+});
+var viewRoot = path.join(__dirname, 'www');
+app.viewRoot = viewRoot;
+//EJS模版引擎的配置
+EJSRender(app, {
+  root: viewRoot,
+  viewExt: 'html',
+  cache: false,
+  debug: true
+});
+//cookie的加密串
+app.keys = ['HBEC'];
+app.use(session({
+  //session 名称
+  key: 'HBEC_SESSIONID',
+  //过期时间:半个小时
+  maxAge: 30 * 60 * 1000
+}, app));
+app.use(bodyParser());
+app.use(json());
+app.use(cors());
+app.use(appRouter);
+
+//处理页面未找到异常
+app.use(function*(next) {
+  if (this.status == 404) {
+    this.body = '404'
+  } else {
+    yield next;
+  }
+});
+
+//监听服务端异常信息
+app.on('error', function (err) {
+  debug(err);
+});
+module.exports = app;
 
